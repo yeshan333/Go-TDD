c10e7cc25db9594f3497cbfd1570f46e0c2424ad
yeshan333 <1329441308@qq.com>
2020-07-05 20:05:28 +0800

    feat(concurrency): goroutine , channel, sendstatement

--- concurrency/check_website.go
+++ concurrency/check_website.go
@@ -1,13 +1,29 @@
 package concurrency
 
+// WebsiteChecker URL 器
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
+// CheckWebsites : 使用 WebsiteChecker 检查 urls slice
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
-	result := make(map[string]bool)
+	results := make(map[string]bool)   // 存储检查结果的 map
+	resultChannel := make(chan result) // 创建一个 result 的 channel
 
 	for _, url := range urls {
-		result[url] = wc(url)
+		go func(u string) {
+			// 引入 channel 避免 race condition
+			resultChannel <- result{u, wc(u)}
+		}(url)
 	}
 
-	return result
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
+
+	return results
 }
